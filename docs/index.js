861a53c9a7fd9dde979ff30af8642d0eb7c573ef
IamBrocco <iambrocco@programmer.net>
2023-02-25 10:03:49 +0200

    added dashboard protoype

---
+++ docs/index.js
@@ -0,0 +1,25 @@
+var commands = document.getElementById("commands");
+var features = document.getElementById("features");
+var dashboard = document.getElementById("dashboard");
+var contribute = document.getElementById("contribute");
+var support = document.getElementById("support");
+
+commands.onclick = () => {
+  window.location.href = "commands";
+};
+
+features.onclick = () => {
+  window.location.href = "features";
+};
+
+dashboard.onclick = () => {
+  window.location.href = "dashboard";
+};
+
+contribute.onclick = () => {
+  window.location.href = "contribute";
+};
+
+support.onclick = () => {
+  window.location.href = "support";
+};

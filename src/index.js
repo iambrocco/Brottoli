fa17080761f534aedc089b9c46351152d7d892a4
IamBrocco <iambrocco@programmer.net>
2023-11-03 19:48:45 +0200

    Test

--- src/index.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config()
-const Client = require("./Structures/Client")
-const client = new Client({intents: ["GuildMessages", "GuildMembers", "MessageContent", "Guilds"]})
-
-client.start(process.env.token)

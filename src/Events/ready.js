3f3a5615a9e2486bdadfe06eb5889223f922c92e
IamBrocco <iambrocco@programmer.net>
2023-11-24 15:00:55 +0200

    Made the last change variable

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -2,16 +2,26 @@ const { ActivityType } = require("discord.js");
 const Client = require("../Structures/Client");
 
 module.exports = {
-    name: "ready",
-    /**
-     * 
-     * @param {Client} client 
-     */
-    async execute(client) {
-        console.log(`${client.user.tag} is ready!`);
-        let customStatusChannelId = "1177592660278640701"
-        let lastMessageId = client.channels.cache.get(customStatusChannelId).lastMessageId
-        let customStatus = (await client.channels.cache.get(customStatusChannelId).messages.fetch(lastMessageId)).content
-        client.user.setPresence({activities: [{name: `${customStatus}`, type: ActivityType.Custom}]})
-    }
-}
+  name: "ready",
+  /**
+   *
+   * @param {Client} client
+   */
+  async execute(client) {
+    console.log(`${client.user.tag} is ready!`);
+    setInterval(async () => {
+      let customStatusChannelId = "1177592660278640701";
+      let lastMessageId = client.channels.cache.get(
+        customStatusChannelId
+      ).lastMessageId;
+      let customStatus = (
+        await client.channels.cache
+          .get(customStatusChannelId)
+          .messages.fetch(lastMessageId)
+      ).content;
+      client.user.setPresence({
+        activities: [{ name: `${customStatus}`, type: ActivityType.Custom }],
+      });
+    }, 5000);
+  },
+};

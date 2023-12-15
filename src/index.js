8d36185098d7d15e3bf116493ddf43d4e12e0c17
IamBrocco <iambrocco@programmer.net>
2023-12-15 23:38:30 +0200

    small update teaser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const Client = require("./Structures/Client");
 const client = new Client({
-  intents: ["GuildMessages", "GuildMembers", "MessageContent", "Guilds"],
+  intents: ["GuildMessages", "GuildMembers", "MessageContent", "Guilds", "DirectMessages"],
 });
+
 client.start(process.env.token);
 

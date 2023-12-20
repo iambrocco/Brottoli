5cf95d8d3eb9f88e4ebe7ab6bfe5adfe24969e7f
IamBrocco <iambrocco@programmer.net>
2023-12-20 16:46:17 +0200

    stupid host!

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,8 +1,6 @@
 const { ActivityType } = require("discord.js");
 const Client = require("../Structures/Client");
-const express = require("express");
-const app = express();
-const port = 3000;
+
 module.exports = {
   name: "ready",
   /**
@@ -10,13 +8,7 @@ module.exports = {
    * @param {Client} client
    */
   async execute(client) {
-    app.get("/", (req, res) => {
-      res.send("Hello World!");
-    });
 
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
     console.log(`${client.user.tag} is ready!`)
       client.user.setPresence({
         activities: [{ name: `/help`, type: ActivityType.Playing }],
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 const Client = require("./Structures/Client");
+const express = require("express");
+const app = express();
+const port = 3000;
 const client = new Client({
   intents: ["GuildMessages", "GuildMembers", "MessageContent", "Guilds", "DirectMessages"],
 });
 
 client.start(process.env.token);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

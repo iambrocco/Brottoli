c16e3e15ebe06bb9c9e678a8f3cb71d154390804
IamBrocco <iambrocco@programmer.net>
2024-02-23 10:34:40 +0200

    Added Colors Support (other than hex)

--- src/Commands/color.js
+++ src/Commands/color.js
@@ -2,6 +2,7 @@ const CommandBuilder = require("../Structures/CommandBuilder.js");
 const CommandTypes = require("../Structures/Enums/CommandTypes.js");;
 const { EmbedBuilder } = require("discord.js");
 const canvasImport = require("@napi-rs/canvas");
+const namedColors = require("color-name-list");
 
 module.exports = {
   data: new CommandBuilder()
@@ -14,11 +15,54 @@ module.exports = {
     .setType(CommandTypes.SLASH),
   async execute(interaction, args) {
     await interaction.deferReply();
+    function colorToHex(color) {
+      function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+      }
+      // Remove whitespace and convert to lowercase
+      color = color.trim().toLowerCase();
 
+      // Hex shorthand (e.g., #fff)
+      if (color.charAt(0) === "#") {
+        // Expand shorthand
+        if (color.length === 4) {
+          color =
+            "#" +
+            color.charAt(1) +
+            color.charAt(1) +
+            color.charAt(2) +
+            color.charAt(2) +
+            color.charAt(3) +
+            color.charAt(3);
+        }
+        return color;
+      }
+
+
+      if (RegExp(`[g-z]`).test(color)) {
+        let someNamedColor = namedColors.find(colorParam => colorParam.name.toLowerCase() == color);
+        return someNamedColor.hex
+      }
+
+      // RGBA format or RGB format
+      if (color.startsWith("rgba") || color.startsWith("rgb")) {
+        let rgba = color
+          .slice(color.indexOf("(") + 1, color.indexOf(")"))
+          .split(",");
+        let r = parseInt(rgba[0].trim());
+        let g = parseInt(rgba[1].trim());
+        let b = parseInt(rgba[2].trim());
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+      }
+
+      // Unsupported format
+      return null;
+    }
     let color = interaction.options.getString("color");
     let colorEmbed = new EmbedBuilder();
     let generatedColor = color ?? generateRGB();
-    let colorImg = await generateCanvas(generatedColor);
+    let colorImg = await generateCanvas(colorToHex(generatedColor));
     async function generateCanvas(color) {
       let canvas = canvasImport.createCanvas(128, 128);
       let ctx = canvas.getContext("2d");
--- src/Commands/gradient.js
+++ src/Commands/gradient.js
@@ -2,6 +2,7 @@ const CommandBuilder = require("../Structures/CommandBuilder.js");
 const CommandTypes = require("../Structures/Enums/CommandTypes.js");
 const javascriptColorGradient = require("javascript-color-gradient");
 const canvasImport = require("@napi-rs/canvas");
+const namedColors = require("color-name-list");
 const { EmbedBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new CommandBuilder()
@@ -25,6 +26,13 @@ module.exports = {
         .setDescription("How many colors should the generated gradient have?")
         .setRequired(true)
     )
+    .addBooleanOption((option) =>
+      option
+        .setName("colortext")
+        .setDescription(
+          "Write The Color Hex Value on the color? (default true)"
+        )
+    )
     .setType(CommandTypes.SLASH)
     .setCategory("Miscellaneous"),
   /**
@@ -32,6 +40,51 @@ module.exports = {
    * @param {import("discord.js").Interaction} interaction
    */
   async execute(interaction) {
+    function colorToHex(color) {
+      function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+      }
+      // Remove whitespace and convert to lowercase
+      color = color.trim().toLowerCase();
+
+      // Hex shorthand (e.g., #fff)
+      if (color.charAt(0) === "#") {
+        // Expand shorthand
+        if (color.length === 4) {
+          color =
+            "#" +
+            color.charAt(1) +
+            color.charAt(1) +
+            color.charAt(2) +
+            color.charAt(2) +
+            color.charAt(3) +
+            color.charAt(3);
+        }
+        return color;
+      }
+
+
+      if (RegExp(`[g-z]`).test(color)) {
+        let someNamedColor = namedColors.find(colorParam => colorParam.name.toLowerCase() == color);
+        return someNamedColor.hex
+      }
+
+      // RGBA format or RGB format
+      if (color.startsWith("rgba") || color.startsWith("rgb")) {
+        let rgba = color
+          .slice(color.indexOf("(") + 1, color.indexOf(")"))
+          .split(",");
+        let r = parseInt(rgba[0].trim());
+        let g = parseInt(rgba[1].trim());
+        let b = parseInt(rgba[2].trim());
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+      }
+
+      // Unsupported format
+      return null;
+    }
+
     function invertColor(hexColor) {
       // Remove # if it's there
       hexColor = hexColor.replace("#", "");
@@ -61,11 +114,13 @@ module.exports = {
     let startColor = interaction.options.getString("start");
     let finishColor = interaction.options.getString("finish");
     let midPoints = interaction.options.getNumber("count");
+    let colorText = interaction.options.getBoolean("colortext") ?? true;
     function generateGradientCanvas(startColor, finishColor, midPoints) {
       let gradient = new javascriptColorGradient()
-        .setColorGradient(startColor, finishColor)
+        .setColorGradient(colorToHex(startColor), colorToHex(finishColor))
         .setMidpoint(midPoints)
         .getColors();
+      gradient.unshift(colorToHex(startColor));
       let canvasSectionheight = canvas.height / gradient.length;
       let colorsString = "";
       for (color in gradient) {
@@ -76,12 +131,14 @@ module.exports = {
           canvas.width,
           canvasSectionheight
         );
-        ctx.fillStyle = invertColor(gradient[color]);
-        ctx.fillText(
-          gradient[color],
-          canvas.width / 2.25,
-          color * canvasSectionheight
-        );
+        if (colorText) {
+          ctx.fillStyle = invertColor(gradient[color]);
+          ctx.fillText(
+            gradient[color],
+            canvas.width / 2.25,
+            color * canvasSectionheight
+          );
+        }
       }
       return { canvas: canvas, gradient: gradient, colorsString: colorsString };
     }
@@ -102,7 +159,7 @@ module.exports = {
         name: "Gradient Info",
         value: `Gradient Start: ${
           generatedCanvasWithGradient.gradient[0]
-        }\nGradient Finish: ${generatedCanvasWithGradient.gradient.pop()}\nGradient Points Count: ${midPoints}`,
+        }\nGradient Finish: ${generatedCanvasWithGradient.gradient[generatedCanvasWithGradient.gradient.length - 1]}\nGradient Points Count: ${midPoints}`,
       });
     await interaction.editReply({
       embeds: [gradientEmbed],
